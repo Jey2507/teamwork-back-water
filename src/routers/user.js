a95eaad009e2972a15a74fe14dfecf64f3f7702c
LesyaKot <lesyakot83@gmail.com>
2024-08-13 17:23:03 +0300

    upd swagger(register), add getAllUsers
    add bearer to login, logout, refresh

--- src/routers/user.js
+++ src/routers/user.js
@@ -3,14 +3,18 @@ import multer from 'multer';
 import {authenticate} from '../middlewares/authenticate.js';
 import { getUserController } from '../controllers/user.js';
 import { patchUserController } from '../controllers/user.js';
+import { getAllUsersController } from '../controllers/user.js';
+import ctrlWrapper from '../utils/ctrlWrapper.js';
 
 
 const userRouter = Router();
 
 const upload = multer({ dest: 'uploads/' });
 
-userRouter.get('/', authenticate, getUserController);
+userRouter.get('/', authenticate, ctrlWrapper(getUserController));
 
-userRouter.patch('/update', authenticate, upload.single('avatar'), patchUserController);
+userRouter.patch('/update', authenticate, upload.single('avatar'), ctrlWrapper(patchUserController));
+
+userRouter.get('/all', authenticate, ctrlWrapper(getAllUsersController));
 
 export default userRouter;
